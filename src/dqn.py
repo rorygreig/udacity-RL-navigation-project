458041827da11b193e158c6f51121a0825323ed5
rory-improbable <rory@improbable.io>
2020-09-18 10:13:57 +0100

    move to src directory

--- src/dqn.py
+++ src/dqn.py
@@ -4,7 +4,7 @@ import numpy as np
 from collections import deque
 import matplotlib.pyplot as plt
 
-from dqn_agent import Agent
+from src.dqn_agent import Agent
 
 agent = Agent(state_size=8, action_size=4, seed=0)
 
